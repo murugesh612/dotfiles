e1e8f2661a80db0a72acb870283605bbb6aae99d
murugesh <murugesan.s0612@gmail.com>
2023-03-11 19:28:25 +0530

    customize nvim

---
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,9 @@
+-- disable plugins here
+return {
+  --diable indent guides
+  { "lukas-reineke/indent-blankline.nvim", enabled = false },
+  { "echasnovski/mini.indentscope", enabled = false },
+
+  -- statusline component that uses lsp to show current code status
+  { "SmiteshP/nvim-navic", enabled = false },
+}
